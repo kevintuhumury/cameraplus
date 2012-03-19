5cba33df13ea26cbb207ce23ecdcb6596dd12772
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-03-19 23:31:01 +0100

    Added checks on the response hash for a User.

--- lib/cameraplus/user.rb
+++ lib/cameraplus/user.rb
@@ -16,7 +16,7 @@ module Cameraplus
 
     def parse
       parse_user
-      parse_pages
+      parse_pages if has_pages?
     end
 
     def parse_user
@@ -24,12 +24,32 @@ module Cameraplus
       @username    = @data.user.username
       @name        = @data.user.realname
       @avatar      = @data.user.avatar
-      @page_count  = @data.user.pages.to_i
-      @photo_count = @data.user.pictures.to_i
+      @page_count  = parsed_page_count
+      @photo_count = parsed_photo_count
     end
 
     def parse_pages
-      @pages ||= @data.pages.map { |page| Page.new(page) }
+      @pages ||= @data.pages.map { |page| Page.new page }
+    end
+
+    def parsed_page_count
+      @data.user.pages.to_i if has_page_count?
+    end
+
+    def parsed_photo_count
+      @data.user.pictures.to_i if has_photo_count?
+    end
+
+    def has_pages?
+      @data.has_key? "pages"
+    end
+
+    def has_page_count?
+      @data.user.has_key? "pages"
+    end
+
+    def has_photo_count?
+      @data.user.has_key? "pictures"
     end
 
   end

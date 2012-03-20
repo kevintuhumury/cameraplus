726b08b1bb432869650fcfca111128256d65dc8d
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-03-20 08:42:33 +0100

    Added the exif data and recipes to the Photo class.

--- lib/cameraplus/photo.rb
+++ lib/cameraplus/photo.rb
@@ -1,15 +1,48 @@
 module Cameraplus
   class Photo
 
-    attr_reader :small, :medium, :large, :width, :height, :location
+    attr_reader :small, :medium, :large, :width, :height, :location, :recipes, :exif
 
     def initialize(data)
-      @small    = data["120px"]
-      @medium   = data["480px"]
-      @large    = data["800px"]
-      @width    = data.fullwidth.to_i
-      @height   = data.fullheight.to_i
-      @location = data.location
+      @data = data
+      parse
+    end
+
+    private
+
+    def parse
+      parse_photo
+      parse_recipes if has_recipes?
+      parse_exif if has_exif?
+    end
+
+    def parse_photo
+      @small    = @data["120px"]
+      @medium   = @data["480px"]
+      @large    = @data["800px"]
+      @width    = @data.fullwidth.to_i
+      @height   = @data.fullheight.to_i
+      @location = @data.location if has_location?
+    end
+
+    def parse_recipes
+      @recipes ||= @data.recipe.map { |recipe| PhotoRecipe.new recipe }
+    end
+
+    def parse_exif
+      @exif ||= @data.exifdata.map { |exif| PhotoExif.new exif }
+    end
+
+    def has_recipes?
+      @data.has_key? "recipe"
+    end
+
+    def has_exif?
+      @data.has_key? "exifdata"
+    end
+
+    def has_location?
+      @data.has_key? "location"
     end
 
   end
--- spec/cameraplus/photo_spec.rb
+++ spec/cameraplus/photo_spec.rb
@@ -4,39 +4,79 @@ describe Cameraplus::Photo do
 
   let(:data) do
     {
-      "800px"     => "http:\/\/pics.campl.us\/f\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
-      "120px"     => "http:\/\/pics.campl.us\/t\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
-      "480px"     => "http:\/\/pics.campl.us\/iphone\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
-      :fullwidth  => 800,
-      :fullheight => 590,
-      :location   => "45.43383498680353 12.34226474539595"
+      "800px"      => "http:\/\/pics.campl.us\/f\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
+      "120px"      => "http:\/\/pics.campl.us\/t\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
+      "480px"      => "http:\/\/pics.campl.us\/iphone\/0\/0b497f01791c851db1a17f81e0621a5c.jpg",
+      "fullwidth"  => 800,
+      "fullheight" => 590,
+      "location"   => "45.43383498680353 12.34226474539595",
+      "recipe"     => [
+        {
+          "type"   => "fx",
+          "value"  => "Vibrant"
+        }
+      ],
+      "exifdata"   => [
+        {
+          "title"  => "Camera",
+          "value"  => "Apple iPhone 4S",
+          "style"  => "main"
+        }
+      ]
     }
   end
 
   subject { Cameraplus::Photo.new data }
 
-  it "has a small version" do
-    subject.small.should eq "http://pics.campl.us/t/0/0b497f01791c851db1a17f81e0621a5c.jpg"
+  context "instance" do
+
+    it "has a small version" do
+      subject.small.should eq "http://pics.campl.us/t/0/0b497f01791c851db1a17f81e0621a5c.jpg"
+    end
+
+    it "has a medium version" do
+      subject.medium.should eq "http://pics.campl.us/iphone/0/0b497f01791c851db1a17f81e0621a5c.jpg"
+    end
+
+    it "has a large version" do
+      subject.large.should eq "http://pics.campl.us/f/0/0b497f01791c851db1a17f81e0621a5c.jpg"
+    end
+
+    it "has a width" do
+      subject.width.should eq 800
+    end
+
+    it "has a height" do
+      subject.height.should eq 590
+    end
+
+    it "has a location" do
+      subject.location.should eq "45.43383498680353 12.34226474539595"
+    end
+
   end
 
-  it "has a medium version" do
-    subject.medium.should eq "http://pics.campl.us/iphone/0/0b497f01791c851db1a17f81e0621a5c.jpg"
+  context "#recipes" do
+
+    it "should have recipes" do
+      subject.recipes.should be_an Array
+    end
+
+    it "should contain a Cameraplus::PhotoRecipe" do
+      subject.recipes.first.should be_a Cameraplus::PhotoRecipe
+    end
+
   end
 
-  it "has a large version" do
-    subject.large.should eq "http://pics.campl.us/f/0/0b497f01791c851db1a17f81e0621a5c.jpg"
-  end
+  context "#exif" do
 
-  it "has a width" do
-    subject.width.should eq 800
-  end
+    it "should have exif data" do
+      subject.exif.should be_an Array
+    end
 
-  it "has a height" do
-    subject.height.should eq 590
-  end
+    it "should contain a Cameraplus::PhotoExif" do
+      subject.exif.first.should be_a Cameraplus::PhotoExif
+    end
 
-  it "has a location" do
-    subject.location.should eq "45.43383498680353 12.34226474539595"
   end
-
 end

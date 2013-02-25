ab7ad53b71ccf202ba0efab2e82c4bf2c170f50a
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-02-25 23:37:43 +0100

    Moved the VCR settings to the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require "cameraplus"
-require "vcr_config"
+require 'vcr'
+
+VCR.configure do |c|
+  c.ignore_localhost     = true
+  c.cassette_library_dir = "spec/vcr_cassettes"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
--- spec/vcr_config.rb
+++
@@ -1,8 +0,0 @@
-require 'vcr'
-
-VCR.configure do |c|
-  c.ignore_localhost     = true
-  c.cassette_library_dir = "spec/vcr_cassettes"
-  c.hook_into :webmock
-  c.configure_rspec_metadata!
-end
